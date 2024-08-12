133a0fc41e3d97e7bdc7602d98deb74b4326c21e
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2024-08-12 11:47:21 -0300

    Commit DataTypes

---
+++ Progamação Em Python/DataType.py
@@ -0,0 +1,11 @@
+name = "Carlos" ;
+number1 = 40;
+number2 = 09.95 ;
+reality = True;
+Ia = False;
+
+print("Name i's", type(name));
+print("Number1 i's", type(number1));
+print("Number2 i's", type(number2));
+print("Reality i's", type(reality));
+print("Ia i's", type(Ia));
