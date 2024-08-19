8dc55d9ada115328144197e87d6ee33eb8c76102
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2024-08-19 10:13:32 -0300

    Update DataType.py

--- Programming WIth Python/DataType.py
+++ Programming WIth Python/DataType.py
@@ -3,9 +3,29 @@ number1 = 40;
 number2 = 09.95 ;
 reality = True;
 Ia = False;
+byte =b"Hello"; 
+dict = {"name" : "John", "age" : 36}	
+sett = {"apple", "banana", "cherry"}
+frozen = frozenset({"apple", "banana", "cherry"});
+byteArray = bytearray(5)
+memorie = memoryview(bytes(5))
+list = ["apple", "banana", "cherry"]; 	
+tuplle = ("apple", "banana", "cherry")
+rangee = range(6)
+Nothing = None;
 
 print("Name i's", type(name));
 print("Number1 i's", type(number1));
 print("Number2 i's", type(number2));
 print("Reality i's", type(reality));
 print("Ia i's", type(Ia));
+print("Byte i's", type(byte));
+print("Dict i's", type(dict));
+print("Set i's", type(sett));
+print("Frozen i's", type(frozen));
+print("Bytes Array i's", type(byteArray));
+print("MemoryView i's", type(memorie));
+print("List i's", type(list));
+print("Tuple i's", type(tuplle));
+print("Range i's", type(rangee));
+print("None i's", type(Nothing));
