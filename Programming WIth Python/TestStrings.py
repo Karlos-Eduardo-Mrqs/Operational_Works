b74093e9d1e9c8c586197f5c2051afc5c42f27f3
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2024-08-19 10:31:09 -0300

    Create TestStrings.py

---
+++ Programming WIth Python/TestStrings.py
@@ -0,0 +1,25 @@
+name = input("What's your name ?:");
+print("Hello",name);
+
+Upper = name.upper();
+Lower = name.lower();
+RemoveSpace = name.strip();
+Locat = name.replace("","|");
+
+print("Your name in UpperCase:",Upper);
+print("Your name in LowerCase:",Lower);
+print("Your name with no spaces:",RemoveSpace);
+print("Your name with replaces:",Locat);
+
+First = "Hello";
+Second = "For";
+Third = "World";
+
+Phrase = First + Second + Third;
+print(Phrase);
+
+Phrase = f"First + Second + Third + {name}";
+print(Phrase);
+
+Phrase = First + Second + Third + name;
+print(Phrase);
