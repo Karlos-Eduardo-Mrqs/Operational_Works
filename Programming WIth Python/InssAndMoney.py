53b4e56c802a0a28f4d59bbfed4d702f69323718
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2024-08-25 18:02:05 -0300

    Update InssAndMoney.py

--- Programming WIth Python/InssAndMoney.py
+++ Programming WIth Python/InssAndMoney.py
@@ -1,5 +1,3 @@
-# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:salário bruto. quanto pagou ao INSS. quanto pagou ao sindicato. o salário líquido. calcule os descontos e o salário líquido, conforme a tabela abaixo:
-
 wage = input("Write your wage per hour:");
 hours = input("Write whats hours you work:");
 brute_wage = float(wage) * float(hours);
