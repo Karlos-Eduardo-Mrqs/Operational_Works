3a2bdb382439ecfac9113a7fb6e747724143d58f
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2024-08-27 16:44:42 -0300

    Update Jumps.py

--- Programming WIth Python/Jumps.py
+++ Programming WIth Python/Jumps.py
@@ -1,10 +1,12 @@
-Athelets = [];
-
 Many_Athelets = int(input("How many Athelets Are Participate ?:"));
-
+Athelets = [];
 for Index in range(Many_Athelets) :
     One_Athelet = input(f"{Index + 1}º Name of Athelet:") ;
     Distance = float(input(f"How far did {One_Athelet} jump ?:"));
-    Athelets.append(f"{Index}º Athelet Name{One_Athelet} And Distance Jump {Distance} ;");
+    Athelets.append((One_Athelet,Distance));
 
-# Em Breve Mais Adições !
+Athletes_Sorted = sorted(Athelets, key=lambda x: x[1], reverse=True);
+Rank_Cont = 0;
+for athlete in Athletes_Sorted:
+    Rank_Cont += 1;
+    print(f"{Rank_Cont}º Athlete: {athlete[0]}, Jump Distance: {athlete[1]} meters");
