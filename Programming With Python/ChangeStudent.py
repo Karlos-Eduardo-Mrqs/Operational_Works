3a91b4f7aabbce0c6b97068d8da2d51b5afe5630
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2024-08-31 22:28:43 -0300

    Create ChangeStudent.py

---
+++ Programming With Python/ChangeStudent.py
@@ -0,0 +1,5 @@
+from random import choice
+Students_Names = ["Carlos","Vitor","Leonardo","Yan"];
+Size = len(Students_Names);
+Chosen = choice(Students_Names);
+print(" The Student Are Chosen For Clear Frame Is {}".format(Chosen))
