ab9f54ef83ed0936d718ffcf49c74d1363345dd0
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2024-09-03 12:10:54 -0300

    Delete Programming With Python directory

--- Programming With Python/ConvertTypes.py
+++
@@ -1,6 +0,0 @@
-IntNum = int(input("Digite um número inteiro:"));
-FloatNum = float(input("Digite um número real:"));
-BoolString = bool(input("Digite um valor booleano(True ou False):"));
-print(type(IntNum));
-print(type(FloatNum));
-print(type(BoolString));
--- Programming With Python/Media5.py
+++
@@ -1,7 +0,0 @@
-Media = 0;
-for I in range(1,5):
-    Nota = float(input(f"Digite a {I}º Nota: "))
-    Media += Nota; 
-
-Media/= I ;
-print(f"A Media do aluno : {Media}");
