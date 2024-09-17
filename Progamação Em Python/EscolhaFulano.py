a7744cf3505a10dacf8a8c9ed9b99b5c6162a8c6
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2024-09-17 10:27:30 -0300

    Create EscolhaFulano.py

---
+++ Progamação Em Python/EscolhaFulano.py
@@ -0,0 +1,5 @@
+from random import choice
+Nomes = ["Carlos","Vitor","Leonardo","Yan"];
+Tamanho = len(Nomes);
+Escolhi = choice(Nomes);
+print(" O estudante que foi escolhido para limpar o quadro foi {}".format(Escolhi));
