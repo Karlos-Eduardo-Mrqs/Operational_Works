e62a71cba78fef07a9ef5f62a2acfb3f93b813d2
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2025-01-09 14:00:48 -0300

    First Programms With Java

---
+++ Programming With Java/Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+    public static void main(String[] args) { 
+        System.out.println("Hello World !");
+        System.out.println("Now, I have codes with Java my Dears !");
+    }
+}
---
+++ Programming With Java/Variable.java
@@ -0,0 +1,8 @@
+public class Variable{
+    public static void main(String[] args){
+        String name = "Carlos Eduardo" ;
+        int age = 19;
+        double height = 1.85;
+        System.out.println("Hello my Name is "+ name + ", have "+ age + " and "+ height + " cm");
+    }
+}
