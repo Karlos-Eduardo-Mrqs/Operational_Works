6c4757d86d504a24ccbebe5c99aa5509810bc21d
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2025-01-09 16:09:18 -0300

    Programms With Operators

---
+++ Programming With Java/Operator.java
@@ -0,0 +1,7 @@
+public class Operator {
+    public static void main(String[] args){
+        int number1 = 5;
+        int number2 = 8;
+        System.out.println(number1+number2);
+    }
+}
---
+++ Programming With Java/OperatorTw.java
@@ -0,0 +1,8 @@
+public class OperatorTw {
+    public static void main(String[] args) {
+        double amount = 9.000;
+        double price = 60;
+        double result = amount/price;
+        System.out.println(result); 
+    }
+}
