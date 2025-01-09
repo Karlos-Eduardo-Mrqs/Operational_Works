69b0d48e40e2caf1559e9106b53cac6748ad4a12
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2025-01-09 16:28:17 -0300

    Scanner Programm

---
+++ Programming With Java/FirstScanner.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class FirstScanner {
+    public static void main(String[] args) {
+        Scanner put = new Scanner(System.in);
+        String nome = put.nextLine();
+        System.out.println("Name:"+nome);
+    }
+}
