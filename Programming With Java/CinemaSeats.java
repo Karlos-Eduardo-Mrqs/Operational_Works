e0f81883e6e6ac75059ad06f35df09e8eb2574b0
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2025-01-12 16:27:35 -0300

    Create CinemaSeats.java

---
+++ Programming With Java/CinemaSeats.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CinemaSeats {
+    
+    public static void main(String[] args) {
+        int[][] seats = {
+        {0, 0, 0, 1, 1, 1, 0, 0, 1, 1},
+        {1, 1, 0, 1, 0, 1, 1, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
+        {0, 1, 1, 1, 0, 0, 0, 1, 1, 1}
+        };
+        try (Scanner sc = new Scanner(System.in)) {
+            int row = sc.nextInt();
+            int column = sc.nextInt();
+            int SealSeat = seats[row] [column];
+            if(SealSeat ==  1){
+                System.out.println("Sold");
+            }else{
+                System.out.println("Free");
+            }
+        }
+    }
+
+}
