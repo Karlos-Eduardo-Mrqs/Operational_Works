01f86bede606e58b83c4a261352d3356330bdd18
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2025-03-11 16:20:15 -0300

    Update ClassWorkers.py

--- Programming WIth Python/ClassWorkers.py
+++ Programming WIth Python/ClassWorkers.py
@@ -1,14 +1,15 @@
 from dataclasses import dataclass
+
 @dataclass
-class Trabalhador: #
-    nome:str;
-    cargo:str;
-    salario:float;
-    bonus:float;
 
-    def calcularSalarioTotal(self): #calcula o salário total do funcionário (salário + bônus) e retorna o valor.
-        return self.salario + self.bonus;
+class Trabalhador:
+    nome: str  # Nome do trabalhador
+    cargo: str  # Cargo do trabalhador
+    salario: float  # Salário base do trabalhador
+    bonus: float  # Bônus anual do trabalhador
 
-funcionario1 = Trabalhador(nome="Julio Cesar",cargo="Programador",salario=3500,bonus=500);
-funcionario1.calcularSalarioTotal();
-print(f"O funcionario {funcionario1.nome} é um ${funcionario1.cargo} com um salario de R${funcionario1.calcularSalarioTotal():.2f}");
+    def calcularSalarioTotal(self): # Calcula o salário total do funcionário (salário + bônus) e retorna o valor.
+        return self.salario + self.bonus
+    
+funcionario1 = Trabalhador(nome="Julio Cesar", cargo="Programador", salario=3500, bonus=500) # Criação de uma instância da classe Trabalhador
+print(f"O funcionário {funcionario1.nome} é um {funcionario1.cargo} com um salário de R${funcionario1.calcularSalarioTotal():.2f}") # Exibição dos dados do funcionário usando f-strings
